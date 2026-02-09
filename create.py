b2555b480c67ef2cdd21135d59c0dfebdb564c35
Elizabeth9980 <queenelizabetheke@gmail.com>
2026-02-08 22:16:05 -0800

    Create create.py

---
+++ create.py
@@ -0,0 +1,19 @@
+def manual_purchase():
+    print("--- MANUAL PURCHASE SYSTEM ---")
+    
+    input("Student (You): ") 
+    print("Staff: Good day. What can I do for you?")
+    
+  
+    input("Student (You): ")  
+    print("Staff: The manual costs N3,200. Please pay.")
+    
+    # 3. Payment
+    input("Student (You): ") 
+    print("Staff: Payment received. Here is your manual.")
+    
+    # 4. Closing
+    input("Student (You): ")
+    print("Staff: You're welcome. Goodbye.")
+
+manual_purchase()
